acc39baa7266cf3b2d7feaee7cc7405c32b474bd
Ash Kumar <kumar.ashwin@outlook.com>
2016-06-04 19:31:07 -0400

    Finished 'HTML and CSS Basics' i.e. second module of the course.

--- web-app/TheWorld/src/TheWorld/Startup.cs
+++ web-app/TheWorld/src/TheWorld/Startup.cs
@@ -20,10 +20,9 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"Hello World!: {context.Request.Path}");
-            });
+            //Fetches default static files from wwwroot directory
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }

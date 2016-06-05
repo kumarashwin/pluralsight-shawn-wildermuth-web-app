19b9b0f34122118f20bea7d44b0ebfd81fc8ccde
Ash Kumar <kumar.ashwin@outlook.com>
2016-06-05 13:11:17 -0400

    Finished 'JavaScript' i.e. third module of the course.

---
+++ web-app/TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// site.js
+
+(function () {
+
+    var ele = $("#username");
+    ele.text("Ash");
+
+    var main = $("#main");
+    main.on("mouseover", function () { main.css("background-color", "#888"); });
+    main.on("mouseout", function () { main.css("background-color", ""); });
+
+    /*var menuItems = $("ul.menu li a");
+    menuItems.on("click", function ()
+    {
+        alert($(this).text());
+    });*/
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) { $(this).text("Show Sidebar"); }
+        else { $(this).text("Hide Sidebar"); }
+
+    });
+    
+})();
